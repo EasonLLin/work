92f86c3123c4cc7c87970eff57b9f883c55f9e6c
eason.lin <eason.lin@nextbank.com.tw>
2021-03-30 22:54:39 +0800

    INIT

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,123 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  Container,
+  Box,
+} from '@material-ui/core';
+import styled from 'styled-components';
+import { spacing, flex } from '@material-ui/system';
+
+const StyledContainer = styled(Container)`
+  ${spacing}
+`;
 
 export default function Home() {
+  const [list, setList] = useState([
+    { name: 'Amy', processed: [] },
+    { name: 'Bob', processed: [] },
+    { name: 'Cory', processed: [] },
+    { name: 'Dora', processed: [] },
+  ]);
+  const [processingList, setProcessingList] = useState([
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [count, setCount] = useState(1);
+  const [waitings, setWaitings] = useState(null);
+
+  const findEmpty = () => {
+    for (let i = 0; i < processingList.length; i++) {
+      if (!processingList[i]) {
+        return i;
+      }
+    }
+  };
+
+  const randomTime = () => {
+    // random between 500 - 1500
+    return Math.floor(Math.random() * 1001) + 500;
+  };
+
+  const clearProcessing = async (nextIndex) => {
+    // const nextList = list;
+    console.log('list0', list);
+    const nextProcessedArr = list[nextIndex].processed;
+    nextProcessedArr.push(count);
+    await setProcessingList(
+      processingList.map((item, i) => (i === nextIndex ? null : item))
+    );
+    await setList(
+      list.map((item, i) =>
+        i === nextIndex ? { ...item, processed: nextProcessedArr } : item
+      )
+    );
+  };
+
+  const addProcessing = async (nextIndex, prevCount) => {
+    await setProcessingList(
+      processingList.map((item, i) => (i === nextIndex ? prevCount : item))
+    );
+  };
+
+  const delay = (time) => {
+    return new Promise((res) => setTimeout(res, time));
+  };
+
+  const triggerNext = async () => {
+    const nextIndex = findEmpty();
+    const prevCount = count;
+    await setCount(count + 1);
+
+    if (Number.isInteger(nextIndex) && !waitings) {
+      await addProcessing(nextIndex, prevCount);
+      await delay(randomTime());
+      clearProcessing(nextIndex);
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <StyledContainer p={2} component="span">
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>name</TableCell>
+              <TableCell>processing</TableCell>
+              <TableCell>processed</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {list.map((item, i) => (
+              <TableRow key={item.name}>
+                <TableCell component="th" scope="row">
+                  {item.name}
+                </TableCell>
+                <TableCell>
+                  {processingList[i] ? processingList[i] : 'idle'}
+                </TableCell>
+                <TableCell>
+                  {item.processed.length ? item.processed.join(',') : 'N/A'}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box display="flex" mt={2} justifyContent="space-between">
+        <Box>{`waitings: ${waitings}`}</Box>
+        <Button variant="contained" color="primary" onClick={triggerNext}>
+          {`Next ${count}`}
+        </Button>
+      </Box>
+    </StyledContainer>
+  );
 }
